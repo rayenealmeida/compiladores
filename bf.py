e15f243a5c7cc632af62ebbb1092673a53893cd1
rayenealmeida <almeidarayene@gmail.com>
2025-04-03 07:33:44 -0300

    symbols part

--- bf.py
+++ bf.py
@@ -17,3 +17,23 @@ class BF:
                     self.index -= 1
                 case "+":
                     self.memory[self.index] =  (self.memory[self.index] +1) % 256
+                case "-":
+                    self.memory[self.index] = (self.memory[self.index] -1) % 256
+                case ".":
+                    print(chr(self.memory[self.index]), end = "")
+                case ",":
+                    self.memory[self.index] = ord(click.getchar(echo = True)) % 256
+                case "[":
+                    cmd = []
+                    ooen_paren = 1
+                    for c in chars:
+                        if c == "]":
+                            open_paren -=1
+                        if c == "[":
+                            open_paren += 1
+                        if open_paren ==0:
+                            break
+                        cmd.append(c)
+                    else:
+                        raise SyntaxError
+                
